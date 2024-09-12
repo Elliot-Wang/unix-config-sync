064573722512527c29fef2e0f1a13bb5dbe4e863
王伟昱 <wangweiyu@yueyuechuxing.cn>
2024-09-12 15:32:22 +0800

    feat: save .zsh/*.zsh

--- save-config.sh
+++ save-config.sh
@@ -4,6 +4,12 @@
 [ -f ~/.zshrc ] && cp ~/.zshrc .zshrc
 [ -f ~/.config/neofetch/config.conf ] && cp ~/.config/neofetch/config.conf neofetch
 
+# 确认系统发行版本
+os_name=${uname}
+if $os_name == Darwin; then
+    [ -f  ~/.zsh/alter.zsh ] && cp ~/.zsh/alter.zsh ./.zsh/alter.zsh.mac 
+fi
+
 git add --all
 if [ -z "$1" ]; then
     git commit -m "update at [$(date +'%F %T')]"
--- update.sh
+++ update.sh
@@ -17,3 +17,9 @@ esac
 [ -f .vimrc ] && [ -f ~/.vimrc ] && cp .vimrc ~/.vimrc
 [ -f .zshrc ] && [ -f ~/.zshrc ] && cp .zshrc ~/.zshrc
 [ -f neofetch/config.conf ] && [ -f ~/.config/neofetch/config.conf ] && cp neofetch/config.conf ~/.config/neofetch/config.conf 
+
+# 确认系统发行版本
+os_name=${uname}
+if $os_name == Darwin; then
+    [ -f  ./.zsh/alter.zsh.mac ] && [ -d  ~/.zsh ] && cp ./.zsh/alter.zsh.mac ~/.zsh/alter.zsh 
+fi
