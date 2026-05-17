4bd0a0a7c0ba04c39ac6a033b5509148c43abcc4
wangweiyu <mindyard@163.com>
2026-05-17 12:56:27 +0800

    add zsh tool

---
+++ .zsh/tool.zsh
@@ -0,0 +1,16 @@
+alias ola="ollama"
+alias ccp="claude --dangerously-skip-permissions"
+alias occp="ollama launch claude --model glm-5.1:cloud -- --dangerously-skip-permissions"
+alias tx="tmux -u"
+
+function fsub() {
+    sort $1 $2 $2 | uniq -u
+}
+
+function funi() {
+    sort $1 $2 | uniq
+}
+
+function fint() {
+    sort $1 $2 | uniq -d
+}
